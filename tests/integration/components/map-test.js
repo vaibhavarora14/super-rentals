5874749959638fa1355901fb0ca91149555adbb9
Vaibhav <varora@catalystone.com>
2020-08-03 16:49:10 +0530

    feat: 🎸 use Map in rentals and improve Map component

--- tests/integration/components/map-test.js
+++ tests/integration/components/map-test.js
@@ -1,14 +1,14 @@
-  import { module, test } from 'qunit';
+import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, find } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import ENV from 'super-rentals/config/environment';
 
-module('Integration | Component | map', function(hooks) {
+module('Integration | Component | map', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders a map image for specificed parameters', async function(assert) {
-    await render(hbs `
+  test('it renders a map image for specificed parameters', async function (assert) {
+    await render(hbs`
       <Map
       @lat="37.7797"
       @lng="-122.4184"
@@ -23,7 +23,7 @@ module('Integration | Component | map', function(hooks) {
     assert.dom('.map img').hasAttribute('width', '150');
     assert.dom('.map img').hasAttribute('height', '120');
 
-    let {src} = find('.map img');
+    let { src } = find('.map img');
     let token = encodeURIComponent(ENV.MAPBOX_ACCESS_TOKEN);
 
     assert.ok(src.includes('-122.4184,37.7797,10'), 'the src should include the lng,lat,zoom parameter');
@@ -31,7 +31,7 @@ module('Integration | Component | map', function(hooks) {
     assert.ok(src.includes(`access_token=${token}`), 'the src should include the escaped access token');
   });
 
-  test('the default alt attribute can be overriden', async function(assert) {
+  test('the default alt attribute can be overriden', async function (assert) {
     await render(hbs`<Map
       @lat="37.7797"
       @lng="-122.4184"
@@ -44,7 +44,7 @@ module('Integration | Component | map', function(hooks) {
     assert.dom('.map img').hasAttribute('alt', 'A map of San Francisco');
   });
 
-  test('the src, width and height attributes cannot be overridden', async function(assert) {
+  test('the src, width and height attributes cannot be overridden', async function (assert) {
     await render(hbs`<Map
       @lat="37.7797"
       @lng="-122.4184"
@@ -59,5 +59,45 @@ module('Integration | Component | map', function(hooks) {
     assert.dom('.map img').hasAttribute('src', /^https:\/\/api\.mapbox\.com/, 'the src starts with "https://api.mapbox.com"');
     assert.dom('.map img').hasAttribute('width', '150');
     assert.dom('.map img').hasAttribute('height', '120');
+  });
+
+  test('it updates the `src` attribute when the arguments change', async function (assert) {
+    this.setProperties({
+      lat: 37.7749,
+      lng: -122.4194,
+      zoom: 10,
+      width: 150,
+      height: 120,
+    });
+
+    await render(hbs`<Map
+      @lat={{this.lat}}
+      @lng={{this.lng}}
+      @zoom={{this.zoom}}
+      @width={{this.width}}
+      @height={{this.height}}
+    />`);
+
+    let img = find('.map img');
+
+    assert.ok(img.src.includes('-122.4194,37.7749,10'), 'the src should include the lng,lat,zoom parameter');
+    assert.ok(img.src.includes('150x120@2x'), 'the src should include the width,height and @2x parameter');
+
+    this.setProperties({
+      width: 300,
+      height: 200,
+      zoom: 12
+    });
+
+    assert.ok(img.src.includes('-122.4194,37.7749,12'), 'the src should include the lng,lat,zoom parameter');
+    assert.ok(img.src.includes('300x200@2x'), 'the src should include the width,height and @2x parameter');
+
+    this.setProperties({
+      lat: 47.6062,
+      lng: -122.3321,
+    });
+
+    assert.ok(img.src.includes('-122.3321,47.6062,12'), 'the src should include the lng,lat,zoom parameter');
+    assert.ok(img.src.includes('300x200@2x'), 'the src should include the width,height and @2x parameter');
   });
 });
