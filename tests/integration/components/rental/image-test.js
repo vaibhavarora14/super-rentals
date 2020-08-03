6868b8f74d876135b3e0d1531579e31a2d4a1ca7
Vaibhav <varora@catalystone.com>
2020-08-03 12:52:16 +0530

    feat: 🎸 add larger/smaller image interaction in rental images

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -1,7 +1,8 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+
 
 module('Integration | Component | rental/image', function(hooks) {
   setupRenderingTest(hooks);
@@ -15,5 +16,29 @@ module('Integration | Component | rental/image', function(hooks) {
     assert.dom('.image').exists();
     assert.dom('.image img').hasAttribute('src', '/assets/images/teaching-tomster.png');
     assert.dom('.image img').hasAttribute('alt', 'Teaching Tomster');
+  });
+
+  test("clicking on component toggle it's size", async function(assert) {
+    await render(hbs `
+      <Rental::Image
+        src="/assets/images/teaching-tomster/png"
+        alt="Teaching Tomster"
+      />
+    `);
+
+    assert.dom('button.image').exists();
+
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image small').hasText('View Larger');
+
+    await click('button.image');
+
+    assert.dom('.image').hasClass('large');
+    assert.dom('.image small').hasText('View Smaller');
+
+    await click('button.image');
+
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image small').hasText('View Larger');
   });
 });
